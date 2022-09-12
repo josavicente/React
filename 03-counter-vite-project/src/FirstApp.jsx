265efc77789310a7f4dacd205d22c06ea6dea696
Josa Vicente <jvicenpe@gmail.com>
2022-09-12 20:56:54 +0200

    :sparkles:

---
+++ 03-counter-vite-project/src/FirstApp.jsx
@@ -0,0 +1,3 @@
+export const FirstApp= () => {
+      return <h1>First App</h1>;
+};
--- 03-counter-vite-project/src/main.jsx
+++ 03-counter-vite-project/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
+import { HelloWorldApp, } from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
-            <HelloWorldApp />
+            <FirstApp />
       </React.StrictMode>
 );

a92cf73b77e12cacdc6c4e60aaf92523852fb18c
Josa Vicente <jvicenpe@gmail.com>
2022-09-12 22:51:52 +0200

    :sparkles:

--- 03-counter-vite-project/src/FirstApp.jsx
+++ 03-counter-vite-project/src/FirstApp.jsx
@@ -2,10 +2,15 @@
 // const getResult = () => {
 //       return 'Josa';
 // }
+import PropTypes from 'prop-types';
 
-export const FirstApp= ( {title, message} ) => {
+export const FirstApp = ( {title, message} ) => {
 
-      
       return ( 
       <><h1>{title}</h1><p>{message}</p></>
 )};
+
+FirstApp.propTypes = {
+      title: PropTypes.string.isRequired,
+      message: PropTypes.string
+}
--- 03-counter-vite-project/src/main.jsx
+++ 03-counter-vite-project/src/main.jsx
@@ -7,6 +7,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
-            <FirstApp title="Hola soy Goku!" message="Kakarot!"/>
+            <FirstApp  />
       </React.StrictMode>
 );

6ba25c65a154af6dff22cf6e676f1442e75dc548
Josa Vicente <jvicenpe@gmail.com>
2022-09-15 23:07:58 +0200

    :tada:

--- 06-GifExpertApp/src/GifExpertApp.jsx
+++ 06-GifExpertApp/src/GifExpertApp.jsx
@@ -1,7 +1,19 @@
-export const GifExpertApp = () {
+
+
+//  const apiKey = '0SQ78YHAvJ2yMgLzOGGpwnMJ0N8cV95t';
+//  const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+//  const { data } = await resp.json(); 
+
+export const GifExpertApp = () => {
       return (
             <>
-                  <h1>GifExpertApp</h1>
+            {/* Título */}
+            <h1>GifExpertApp</h1>
+
+            {/* Entrada de datos */}
+
+            {/* Listado de Gif's */}
+                  {/* GifItem */}
             </>
       )
-}
+};

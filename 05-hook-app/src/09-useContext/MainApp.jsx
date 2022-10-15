61116feb71cd4e44bd5c9c60bc52e19c615a8bd8
Josa Vicente <jvicenpe@gmail.com>
2022-10-15 22:09:55 +0200

    useContext

---
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes, Link} from "react-router-dom"
+import { HomePage } from "./HomePage"
+import { AboutPage } from "./AboutPage"
+import { LoginPage } from "./LoginPage"
+import { NavBar } from "./NavBar"
+
+export const MainApp = () => {
+  return (
+    <>
+        <h1>MainApp</h1>
+            {/* <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/about">About</Link> */}
+            <NavBar />
+        <hr/>
+
+        
+        <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="login" element={<LoginPage />}/>
+            <Route path="about" element={<AboutPage />}/>
+
+            {/* <Route path="/*" element={<LoginPage />}/> */}
+
+            <Route path="/*" element={ <Navigate to="/about"/> } />
+        </Routes>
+    </>
+  )
+}
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // import { Padre } from './06-memos/07-tarea-memo/Padre'
 // import { CallBackHook } from './06-memos/CallBackHook'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -12,21 +13,25 @@ import ReactDOM from 'react-dom/client'
 // import { Memorize } from './06-memos/memorize'
 // import { MemorizeHook } from './06-memos/MemorizeHook'
 // import { HooksApp } from './HooksApp'
-import { TodoApp } from './08-useReducer/TodoApp'
-import './index.css'
+import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <CounterApp /> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <FormWithCustomHook/> */}
-    {/* <MultipleCustomHooks /> */}
-    {/* <FocusScreen /> */}
-    {/* <Layout /> */}
-    {/* <Memorize /> */}
-    {/* <MemorizeHook /> */}
-    {/* <CallBackHook /> */}
-    {/* <Padre /> */}
-    <TodoApp />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <React.StrictMode>
+            {/* <CounterApp /> */}
+            {/* <CounterWithCustomHook /> */}
+            {/* <FormWithCustomHook/> */}
+            {/* <MultipleCustomHooks /> */}
+            {/* <FocusScreen /> */}
+            {/* <Layout /> */}
+            {/* <Memorize /> */}
+            {/* <MemorizeHook /> */}
+            {/* <CallBackHook /> */}
+            {/* <Padre /> */}
+            {/* <TodoApp /> */}
+            <MainApp />
+        </React.StrictMode>
+    </BrowserRouter>
+);

74d51e8a2c8ebaf507110b25a9c88a0ceb3fb801
Josa Vicente <jvicenpe@gmail.com>
2022-10-16 21:47:10 +0200

    userProvider-userContext

--- 05-hook-app/src/09-useContext/HomePage.jsx
+++ 05-hook-app/src/09-useContext/HomePage.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext"
 
 export const HomePage = () => {
+
+  const { user } = useContext(UserContext)
     return (
       <>
-          <h1>HomePage</h1>
+          <h1>HomePage, user: <small>{user?.name}</small></h1>
           <hr/>
+          <pre>
+            { JSON.stringify( user, null, 3 ) }
+          </pre>
       </>
     )
   }
--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -3,10 +3,11 @@ import { HomePage } from "./HomePage"
 import { AboutPage } from "./AboutPage"
 import { LoginPage } from "./LoginPage"
 import { NavBar } from "./NavBar"
+import { UserProvider } from "./context/UserProvider"
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
         <h1>MainApp</h1>
             {/* <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
@@ -24,6 +25,6 @@ export const MainApp = () => {
 
             <Route path="/*" element={ <Navigate to="/about"/> } />
         </Routes>
-    </>
+    </UserProvider>
   )
 }
